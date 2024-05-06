02266c088a12049651abc14af2a8566ecf25f4e7
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2024-05-06 14:38:28 +0300

    updated loading

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,12 +3,21 @@ import { Suspense } from 'react';
 
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
+import Loader from 'components/Loader/Loader';
+
+import css from './Layout.module.css';
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Suspense fallback={<p>Loadind...</p>}>
+      <Suspense
+        fallback={
+          <div className={css.loadingWrapper}>
+            <Loader />
+          </div>
+        }
+      >
         <Outlet />
       </Suspense>
       <Footer />
