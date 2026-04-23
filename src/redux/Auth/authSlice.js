9a3bcbc396ce34f5d444cf33b08c8fdcb28172f2
Himadieiev Ruslan <gimadejev86@gmail.com>
2026-04-23 17:46:31 +0300

    Refactor authSlice to use builder syntax for extraReducers.

--- src/redux/Auth/authSlice.js
+++ src/redux/Auth/authSlice.js
@@ -1,39 +1,44 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 import { authInitialState } from './initialState';
-import { currentUserThunk, loginThunk, logoutThunk, registerThunk } from './thunks';
+import {
+  currentUserThunk,
+  loginThunk,
+  logoutThunk,
+  registerThunk,
+} from './thunks';
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState: authInitialState,
-  extraReducers: {
-    [registerThunk.fulfilled](state, action) {
-      state.user = action.payload;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
-    [loginThunk.fulfilled](state, action) {
-      console.log(action);
-      state.user = action.payload;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
-    [logoutThunk.fulfilled](state, action) {
-      state.user = { name: null, email: null };
-      state.token = null;
-      state.isLoggedIn = false;
-    },
-    [currentUserThunk.pending](state, action) {
-      state.isFetchingCurrentUser = true;
-    },
-    [currentUserThunk.fulfilled](state, action) {
-      state.user = action.payload;
-      state.isLoggedIn = true;
-      state.isFetchingCurrentUser = false;
-    },
-    [currentUserThunk.rejected](state, action) {
-      state.isFetchingCurrentUser = false;
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(registerThunk.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(loginThunk.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logoutThunk.fulfilled, state => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+      .addCase(currentUserThunk.pending, state => {
+        state.isFetchingCurrentUser = true;
+      })
+      .addCase(currentUserThunk.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isFetchingCurrentUser = false;
+      })
+      .addCase(currentUserThunk.rejected, state => {
+        state.isFetchingCurrentUser = false;
+      });
   },
 });
 
