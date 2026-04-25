77fa2886955ffa847097e80c455358d89849f097
Himadieiev Ruslan <gimadejev86@gmail.com>
2026-04-26 00:02:39 +0300

    Refactor Filter component with modern styling and search icon.

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,4 @@
-import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
-
 import { setFilter } from 'redux/Filter/filterSlice';
 
 import css from './Filter.module.css';
@@ -8,6 +6,7 @@ import css from './Filter.module.css';
 const Filter = () => {
   const filter = useSelector(state => state.filter.value);
   const dispatch = useDispatch();
+
   const handleChange = e => {
     dispatch(setFilter(e.currentTarget.value));
   };
@@ -15,17 +14,32 @@ const Filter = () => {
   return (
     <div className={css.filter}>
       <label className={css.label}>
-        Find contacts by name
-        <br />
-        <input className={css.input} value={filter} onChange={handleChange} type="text" />
+        <svg
+          className={css.icon}
+          width="18"
+          height="18"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M21 21L16.5 16.5M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+          />
+        </svg>
+        <span>Find contacts by name</span>
       </label>
+      <input
+        className={css.input}
+        value={filter}
+        onChange={handleChange}
+        type="text"
+        placeholder="Search..."
+      />
     </div>
   );
-};
-
-Filter.propTypes = {
-  value: PropTypes.string,
-  onChange: PropTypes.func,
 };
 
 export default Filter;
